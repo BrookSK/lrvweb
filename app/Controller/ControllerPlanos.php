df505c2fc485a937550588416a61ea937d35ccba
Lucas Vacari <lucasrvacari99@gmail.com>
2023-08-14 20:02:55 -0300

    Update site
    Update files from 'lrvweb.com.br'

--- app/Controller/ControllerPlanos.php
+++ app/Controller/ControllerPlanos.php
@@ -7,8 +7,8 @@ use Src\Interfaces\InterfaceView;
 class ControllerPlanos extends ClassRender implements InterfaceView{
     public function __construct(){
         $this->setTitle("Portfolio Lucas Vacari - Planos");
-        $this->setDescription("See my best plans that will please you the most.");
-        $this->setKeywords("portfolio, lucas, vacari, planos, precos, hospedagem, site");
+        $this->setDescription("Veja os melhores preços para a criação de um site profissional.");
+        $this->setKeywords("portfolio, lucas, vacari, planos, precos, hospedagem, site, lrvweb, lrv, web, desenvolvimento, criacao, sites, ecommerce");
         $this->setDir("planos");
         $this->renderLayout();
     }
