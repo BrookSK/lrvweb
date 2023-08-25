d37e5055d59a136604729c441659f5aecbae401f
Lucas Vacari <lucasrvacari99@gmail.com>
2023-08-25 18:15:48 -0300

    Teste

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 #Arquivos diretórios raízes
-$PastaInterna="";
+$PastaInterna="12test/";
 
-define('DIRPAGE',"https://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
+define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
 
 if(substr($_SERVER['DOCUMENT_ROOT'],-1)=='/'){
     define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");
